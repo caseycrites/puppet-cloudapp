3bf2e62c9554a57d5ec5db8d76d3c09cd8ed83ff
Casey Crites <casey@marmaduke.local>
2014-07-10 10:49:03 -0700

    Point at Cloud, not CloudApp.

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,7 +4,7 @@
 #
 #     include cloudapp
 class cloudapp {
-  package { 'CloudApp':
+  package { 'Cloud':
     ensure   => installed,
     provider => 'compressed_app',
     source   => 'http://f.cl.ly.s3.amazonaws.com/builds/CloudApp1.5.4.zip'
--- spec/classes/cloudapp_spec.rb
+++ spec/classes/cloudapp_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'cloudapp' do
   it do
-    should contain_package('CloudApp').with({
+    should contain_package('Cloud').with({
       :provider => 'compressed_app',
       :source   => 'http://f.cl.ly.s3.amazonaws.com/builds/CloudApp1.5.4.zip'
     })
